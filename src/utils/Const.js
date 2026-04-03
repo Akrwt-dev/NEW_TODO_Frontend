447bc69221da6a91381bb5204e8397a5a8433dc4
Akrwt-dev <volcanoak1@gmail.com>
2026-04-03 18:40:31 +0530

    fix Base_url

--- src/utils/Const.js
+++ src/utils/Const.js
@@ -1,2 +1,2 @@
-export const BASE_URL ="https://new-todo-2-ib8k.onrender.com"
+export const BASE_URL ="https://new-todo-1-x6d4.onrender.com"
 export const PROFILE_PIC = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRaWM9V9Ptofm8gIJIgdBysoHHoWw9gSMELnA&s"
