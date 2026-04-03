58ae18d61f9b70208a68f3261f27f9d99b0ffe03
Akrwt-dev <volcanoak1@gmail.com>
2026-04-03 16:49:04 +0530

    added vesal config link

--- src/utils/Const.js
+++ src/utils/Const.js
@@ -1,2 +1,2 @@
-export const BASE_URL ="http://localhost:4000"
+export const BASE_URL ="https://new-todo-2-ib8k.onrender.com"
 export const PROFILE_PIC = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRaWM9V9Ptofm8gIJIgdBysoHHoWw9gSMELnA&s"
